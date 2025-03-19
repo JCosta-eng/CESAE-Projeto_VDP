e2440edd27fa3f2b61a55bc9cfbbc3e37c76ca71
JCosta-eng <jorgecosta.m.eng@gmail.com>
2025-03-19 17:20:43 +0000

    Criacao de diferentes páginas
    Cada página tem agora o seu ficheiro proprio, em views

--- app.py
+++ app.py
@@ -4,11 +4,7 @@
 #### Membros: Bruno Bernardo, Adriano Rodrigues, Jorge Costa.
 ##### 02 de Abril de 2025
 
-# inserir no .gitignore
-#      streamlit.txt
-#      environment-how to.txt
-
-#HOW TO
+#HOW TO ---------------------------------
 ### 1- Criar env
 #run: python -m venv env
 #run: env\Scripts\activate
@@ -16,12 +12,47 @@
 ### 2- Install libraries
 #1 pip install -r requirements.txt
 #2 python.exe -m pip install --upgrade pip
-#3 verificar se está no environment "env", se não, correr env\Scripts\activate
+
+
+####3 
+# verificar se está no environment "env", se não, correr env\Scripts\activate
+# run: 
+
+#-----------------------------------------
 
 #Libraries 
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as pylt
+import matplotlib.pyplot as plt
 import seaborn as sns
-import plotly as plty
+import plotly as px
+import sys
+import os
 
+#Paginas
+sys.path.append(os.path.abspath("views"))
+
+import home
+import plots
+import about
+
+#-----------------------------------------
+#Functions:
+def StreamDash():
+    #Config
+    st.set_page_config(page_title="Dashboard", page_icon="📊", layout="wide")
+
+    #Sidebar
+    st.sidebar.title("Menu")
+    page = st.sidebar.radio("Escolha uma página", ["Home", "Plots and Graphs", "About"])
+
+    #Funcoes paginas
+    if page == "Home":
+        home.show()
+    elif page == "Plots and Graphs":
+        plots.show()
+    elif page == "About":
+        about.show()
+    
+### >>>>>>>>>>>>>> Call Functions <<<<<<<<<<<<<<
+StreamDash()
---
+++ views/plots.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show():
+    st.title("📊 Análise e gráficos 🏎️")
+    st.write("Aqui estão algumas visualizações de dados.")
